22abcaadfc09e9ecd1a2a2c839957cd8bcd09360
slevano1 <stevenlevano1@gmail.com>
2018-02-15 02:01:43 -0800

    added some CSS

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Royale with Cheese', FALSE);
